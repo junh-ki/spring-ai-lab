2d12c239cd736ab90499da80d82c89e23225e2de
Junhyung Ki <j.ki@kosyma.com>
2026-03-28 15:50:35 +0100

    Implement the default conversation memory

--- src/main/java/com/example/springailab/config/ChatClientConfig.java
+++ src/main/java/com/example/springailab/config/ChatClientConfig.java
@@ -1,6 +1,9 @@
 package com.example.springailab.config;
 
 import org.springframework.ai.chat.client.ChatClient;
+import org.springframework.ai.chat.client.advisor.MessageChatMemoryAdvisor;
+import org.springframework.ai.chat.client.advisor.SimpleLoggerAdvisor;
+import org.springframework.ai.chat.memory.ChatMemory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -8,9 +11,16 @@ import org.springframework.context.annotation.Configuration;
 public class ChatClientConfig {
 
     @Bean
-    ChatClient chatClient(final ChatClient.Builder chatClientBuilder) {
+    ChatClient chatClient(final ChatClient.Builder chatClientBuilder,
+                          final ChatMemory chatMemory) {
         return chatClientBuilder
-            .defaultSystem("Your are a helpful assistant.")
+            .defaultSystem("You are a helpful assistant.")
+            .defaultAdvisors(
+                MessageChatMemoryAdvisor
+                    .builder(chatMemory)
+                    .build(),
+                new SimpleLoggerAdvisor()
+            )
             .build();
     }
 }
