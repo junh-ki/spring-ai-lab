273018980b5a9b5b3a3aaf40b32b94d856db3bf1
Junhyung Ki <j.ki@kosyma.com>
2026-03-31 10:22:47 +0200

    Set up AsyncConfig to propagate security context from one thread to another to avoid heisenbugs in prod

---
+++ src/main/java/com/example/springailab/config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.example.springailab.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.TaskExecutor;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.security.task.DelegatingSecurityContextAsyncTaskExecutor;
+
+@EnableAsync
+@Configuration
+public class AsyncConfig {
+
+    @Bean
+    public TaskExecutor taskExecutor() {
+        final ThreadPoolTaskExecutor threadPoolTaskExecutor = new ThreadPoolTaskExecutor();
+        threadPoolTaskExecutor.setCorePoolSize(5);
+        threadPoolTaskExecutor.setMaxPoolSize(10);
+        threadPoolTaskExecutor.setQueueCapacity(25);
+        threadPoolTaskExecutor.initialize();
+        return new DelegatingSecurityContextAsyncTaskExecutor(threadPoolTaskExecutor); // wrapping to propagate security
+    }
+}
