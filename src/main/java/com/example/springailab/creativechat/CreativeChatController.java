50b8217363ae15e3cf4b83ace5f7d4f8293bde82
Junhyung Ki <j.ki@kosyma.com>
2026-04-01 10:29:05 +0200

    Switch Controller annotation to RestController

--- src/main/java/com/example/springailab/creativechat/CreativeChatController.java
+++ src/main/java/com/example/springailab/creativechat/CreativeChatController.java
@@ -1,11 +1,11 @@
 package com.example.springailab.creativechat;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequiredArgsConstructor
 public class CreativeChatController {
 
