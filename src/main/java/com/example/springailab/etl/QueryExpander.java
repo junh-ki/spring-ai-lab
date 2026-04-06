a62120dfd651f5375ddef7e179f880e674306388
Junhyung Ki <j.ki@kosyma.com>
2026-04-06 09:11:15 +0200

    Implement the Decomposition pattern to resolve a multi-part question from user

--- src/main/java/com/example/springailab/etl/QueryExpander.java
+++ src/main/java/com/example/springailab/etl/QueryExpander.java
@@ -11,11 +11,19 @@ import org.springframework.stereotype.Component;
 @RequiredArgsConstructor
 public class QueryExpander {
 
-    private static final String PROMPT_TEMPLATE = """
+    private static final String PROMPT_TEMPLATE_QUERY_EXPANSION = """
         You are an AI search assistant.
         Generate 3 different search queries based on the user question to retrieve relevant documents.
         
         Focus on different aspects: synonyms, technical terms, and related concepts.
+        
+        User Question: {query}
+        
+        {format}
+        """;
+    private static final String PROMPT_TEMPLATE_DECOMPOSITION = """
+        You are an AI search assistant.
+        Beak down this complex question into sub-questions that can be answered individually.
         
         User Question: {query}
         
@@ -26,10 +34,25 @@ public class QueryExpander {
     private final ChatClient chatClient;
 
     public List<String> expand(final String originalQuery) {
+        return prompt(
+            originalQuery,
+            PROMPT_TEMPLATE_QUERY_EXPANSION
+        );
+    }
+
+    public List<String> decompose(final String originalQuery) {
+        return prompt(
+            originalQuery,
+            PROMPT_TEMPLATE_DECOMPOSITION
+        );
+    }
+
+    private List<String> prompt(final String originalQuery,
+                                final String promptTemplate) {
         return this.chatClient.prompt()
             .user(promptUserSpec ->
                 promptUserSpec
-                    .text(PROMPT_TEMPLATE)
+                    .text(PROMPT_TEMPLATE_QUERY_EXPANSION)
                     .param("query", originalQuery)
                     .param("format", BEAN_OUTPUT_CONVERTER.getFormat())
             )
